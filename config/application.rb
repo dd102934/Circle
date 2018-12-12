5a5830f0cffd2434bdf7c89e22477bd6aee73222
Daijiro Igeta <lemon95tennis@gmail.com>
2018-12-12 19:01:44 +0900

    Color change

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,6 @@ module Circle
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('path', 'to', 'ymlfile', '*.{yml}').to_s]
   end
 end
